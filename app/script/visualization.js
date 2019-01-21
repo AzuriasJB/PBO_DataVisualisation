fdfcaccb6b303773d8d26f3beda784a964adb88d
David Wolf <david.wolf98@web.de>
2019-01-21 11:54:40 +0100

    Slider für Auswahl des Jahres hinzugefügt

--- app/script/visualization.js
+++ app/script/visualization.js
@@ -10,6 +10,13 @@ var Visualization = function () {
          */
         draw: function () {
             d3.json("data/geodata.json", function(error, data){
+                var dataTime = d3.range(0, 17).map(function(d) {
+                    return new Date(2000 + d, 10, 3);
+                });
+                var year = "2010";
+                var min = d3.min(dataTime);
+                var max = d3.max(dataTime);
+
                 var width  = window.innerWidth;
                 var height = window.innerHeight;
                 console.log("W: " + width + " / H: " + height);
@@ -58,8 +65,8 @@ var Visualization = function () {
                 var dataContinent = [0,0,0,0];
                 //set colors for regions
                 DataProvider.getPreparedData().forEach(function (region, i) {
-                    var data = DataProvider.getAverageForRegion(region.Region,'2016','22013');
-                    regionColors[i]=new Array(region.Region,ColorScaleYear('22013', data, '2016'));
+                    var data = DataProvider.getAverageForRegion(region.Region,year,'22013');
+                    regionColors[i]=new Array(region.Region,ColorScaleYear('22013', data, year));
                     if (region.Region.includes("Europe")) {
                         dataContinent[0] += data;
                     } else if (region.Region.includes("Africa")) {
@@ -71,21 +78,21 @@ var Visualization = function () {
                     }
                 });
                 for (i = 0; i<dataContinent.length;i++) {
-                    continentColors[i]=ColorScaleYear('22013', dataContinent[i], '2016');
+                    continentColors[i]=ColorScaleYear('22013', dataContinent[i], year);
                 }
                     
                 function handleMouseOver(continent, country) {
                     if(hover){
                         switch(view){
                         case"World":
-                            d3.selectAll('path')
+                            svg.selectAll('path')
                             .filter(function(d) { return d.properties.continent != continent })
                             .transition().duration(100)
                             .attr('opacity', 0.3);
                         break;
                         case"Continent":
                             if (continent == cont){
-                                d3.selectAll('path')
+                                svg.selectAll('path')
                                 .filter(function(d) { return d.properties.name != country})
                                 .filter(function(d) { return d.properties.continent == cont})
                                 .transition().duration(100)
@@ -117,7 +124,7 @@ var Visualization = function () {
                                 .filter(function(d) { return d.properties.name == country})
                                 .append('circle')
                                 .attr('r',function(d){ 
-                                    var tempData = SizeScaleYear('22013', DataProvider.getValuebyiso(d.properties.iso_a3,"2010","22013"), '2010');
+                                    var tempData = SizeScaleYear('22013', DataProvider.getValuebyiso(d.properties.iso_a3,year,"22013"), year);
                                     if (tempData == false || tempData == undefined || tempData !== tempData) {
                                         tempData = 0;
                                     }
@@ -128,7 +135,7 @@ var Visualization = function () {
                                         //console.log(d3.mouse(this)[0]);
                                         //console.log("RETURNING FOR X: "+path.centroid(d)[0])
                                         //return path.centroid(d)[0];
-                                        var tempData = SizeScaleYear('22013', DataProvider.getValuebyiso(d.properties.iso_a3,"2010","22013"), '2010');
+                                        var tempData = SizeScaleYear('22013', DataProvider.getValuebyiso(d.properties.iso_a3,year,"22013"), year);
                                         if (tempData == false || tempData == undefined || tempData !== tempData) {
                                             tempData = 0;
                                         }
@@ -147,9 +154,9 @@ var Visualization = function () {
                                 .attr('stroke','black')
                                 .attr('fill', function(d,i) {
                                     //dynamically change color of the circle diagram
-                                    var circleColor = ColorScaleYear('21032', DataProvider.getValuebyiso(d.properties.iso_a3,"2010","21032"), '2010');
+                                    var circleColor = ColorScaleYear('21032', DataProvider.getValuebyiso(d.properties.iso_a3,year,"21032"), year);
                                     var circlePercent;
-                                    var tempData = DataProvider.getValuebyiso(d.properties.iso_a3,"2010","210041");
+                                    var tempData = DataProvider.getValuebyiso(d.properties.iso_a3,year,"210041");
                                     if (tempData == "<2.5") {
                                         circlePercent = "2.5";
                                     } else if (tempData == false || tempData == undefined)
@@ -170,7 +177,7 @@ var Visualization = function () {
                                         circlePercent = tempData;
                                     }
                                     //console.log(circlePercent);
-                                    d3.selectAll("stop")
+                                    svg.selectAll("stop")
                                         .attr("offset","0%")
                                         .transition()
                                         .style("stop-color", function(d,i) {
@@ -207,12 +214,12 @@ var Visualization = function () {
                     if(hover){
                         switch(view){
                         case"World":
-                            d3.selectAll('path')
+                            svg.selectAll('path')
                                 .transition().duration(300)
                                 .attr('opacity', 1);
                         break;
                         case"Continent":
-                            d3.selectAll('path')
+                            svg.selectAll('path')
                                 .filter(function(d) { return d.properties.continent == cont })
                                 .transition().duration(300)
                                 .attr('opacity', 1);
@@ -236,7 +243,7 @@ var Visualization = function () {
                         view = "Continent";
                         cont = continent;
                         // alle nicht angeklickten Kontinente ausblenden
-                        d3.selectAll('path')
+                        svg.selectAll('path')
                             .filter(function(d) { return d.properties.continent != continent })
                                 .transition().duration(300)
                             .attr('opacity', 0);
@@ -266,13 +273,13 @@ var Visualization = function () {
                         default:
                             break;
                         }
-                        console.log(transformString);
-                        d3.selectAll('path')
+                        //console.log(transformString);
+                        svg.selectAll('path')
                             .filter(function(d) { return d.properties.continent == continent })
                             .transition().duration(300)
                             .attr("transform", transformString)
                             .attr('fill',  function(d, i) { 
-                                return ColorScale ('21032', DataProvider.getValuebyiso(d.properties.iso_a3,"2010","21032"),2010);
+                                return ColorScale ('21032', DataProvider.getValuebyiso(d.properties.iso_a3,year,"21032"),year);
                             });
                         break;
                     case"Continent":
@@ -283,7 +290,7 @@ var Visualization = function () {
                         svg.selectAll('text').remove();
 
                         // alle anderen Kontinente einblenden
-                        d3.selectAll('path')
+                        svg.selectAll('path')
                             .filter(function(d) { return d.properties.continent != cont })
                                 .transition().duration(300)
                             .attr('opacity', 1);
@@ -312,12 +319,12 @@ var Visualization = function () {
                         default:
                             break;
                         }
-                        d3.selectAll('path')
+                        svg.selectAll('path')
                             .filter(function(d) { return d.properties.continent == cont })
                             .transition().duration(300)
                             .attr("transform", transformString)
                             .attr('fill',  function(d, i) {
-                                return ColorScale ('21032', DataProvider.getAverageForContinent(d.properties.continent,"2010","21032"),2010);
+                                return ColorScale ('21032', DataProvider.getAverageForContinent(d.properties.continent,year,"21032"),year);
                             });
                         cont = null;
                         break;
@@ -346,7 +353,7 @@ var Visualization = function () {
                     return d.properties.name;
                     })
                 .attr('fill',  function(d, i) {
-                    return ColorScale ('21032', DataProvider.getAverageForContinent(d.properties.continent,"2010","21032"),2010);
+                    return ColorScale ('21032', DataProvider.getAverageForContinent(d.properties.continent,year,"21032"),year);
                 })
                 .on("mouseover", function(d, i) {
                     handleMouseOver(d.properties.continent, d.properties.name);
@@ -357,6 +364,39 @@ var Visualization = function () {
                 .on("click", function(d, i) {
                     handleMouseClick(d.properties.continent);
                     });
+
+                var sliderTime = d3.sliderBottom()
+                .min(min)
+                .max(max)
+                .step(1000 * 60 * 60 * 24 * 365)
+                .width(600)
+                .tickFormat(d3.timeFormat('%Y'))
+                .tickValues(dataTime)
+                .default(new Date(year, 10, 3))
+                .on("end", val => {
+                    year = d3.timeFormat('%Y')(val);
+                    console.log("New Year is: " + year);
+                    svg.selectAll('path')
+                    .attr('fill',  function(d, i) {
+                        if (year == "2001") {
+                            return "rgb(150,150,150)";
+                        }
+                        if (view=="World") {
+                            return ColorScale ('21032', DataProvider.getAverageForContinent(d.properties.continent,year,"21032"),year);
+                        } else if (view=="Continent") {
+                            return ColorScale ('21032', DataProvider.getValuebyiso(d.properties.iso_a3,year,"21032"),year);
+                        }
+                    });
+                    //drawGeodata();
+                });
+
+                var gTime = d3
+                    .select('div#slider')
+                    .append('svg')
+                    .append('g')
+                    .attr('transform', 'translate(30,30)');
+
+                gTime.call(sliderTime);
             })
             
         }
